35b96ff64800732317f9b10c436c19ef6f1d19fd
Yenifer Daniela Tamayo Franco - Ceiba Software <yenifer.tamayo@ceiba.com.co>
2018-08-06 10:12:17 -0500

    Renombrado

--- src/main/java/com/parqueadero2018/Parqueadero2018Application.java
+++ src/main/java/com/parqueadero2018/Parqueadero2018Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class parqueadero2018Application {
+public class Parqueadero2018Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(parqueadero2018Application.class, args);
+		SpringApplication.run(Parqueadero2018Application.class, args);
 	}
 }
