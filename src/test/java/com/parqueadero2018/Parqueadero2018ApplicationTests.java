dbb8ee7c7338e792c72888f1e11d02fb36df6ce0
Yenifer Daniela Tamayo Franco - Ceiba Software <yenifer.tamayo@ceiba.com.co>
2018-08-06 14:28:05 -0500

    Modificacion unit test

--- src/test/java/com/parqueadero2018/Parqueadero2018ApplicationTests.java
+++ src/test/java/com/parqueadero2018/Parqueadero2018ApplicationTests.java
@@ -1,19 +1,20 @@
 package com.parqueadero2018;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataJpaTest
 public class Parqueadero2018ApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
+	
+	@Autowired
+	Parqueadero2018Application parqueaderoApplication;
 	
 	@Test
 	public void unitTest()
